18b02734f8b02f4bf5938f31aed04f02fa11c24b
ueli kunz <kunz@ideadapt.net>
2026-06-28 21:51:27 +0200

    do not remove page root request path (only a /)

--- report-page_slug.sh
+++ report-page_slug.sh
@@ -23,7 +23,7 @@ find ./logs -name '*-*.tar.gz' |  \
 cat ./logs/$report_name.log \
     | grep --text --invert-match -E '\.(txt|js|php|css|png|gif|jpeg|jpg|webp|svg|env|asp|woff|woff2)' \
     | grep --text --invert-match -E 'preview_id' > ./logs/$report_name-nostatic.log
-cat ./logs/$report_name-nostatic.log | sed 's|/ HTTP| HTTP|' | sed "s|$page_slug/?s=|$page_slug?s=|" > ./logs/$report_name-nostatic-normalized.log
+cat ./logs/$report_name-nostatic.log | sed 's|[^ ]/ HTTP| HTTP|' | sed "s|$page_slug/?s=|$page_slug?s=|" > ./logs/$report_name-nostatic-normalized.log
 cat ./logs/$report_name-nostatic-normalized.log | deno run --reload exclude-bots.ts > ./logs/$report_name-nostatic-normalized-nobot.log
 # remove -o option to render report in terminal
 goaccess ./logs/$report_name-nostatic-normalized-nobot.log -p ./goaccess.conf --html-report-title "$report_name" -o report-$report_name.html
